9a75d4485bd39bc0b24c0c95b2a5432057a3a180
Danylo Pylypenko <p.prince.e777@gmail.com>
2026-09-24 14:20:36 +0300

    fix time of elapse

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import time
 from hashlib import sha256
+from concurrent.futures import ProcessPoolExecutor
 
 
-PASSWORDS_TO_BRUTE_FORCE = [
+PASSWORDS_TO_BRUTE_FORCE = {
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
     "cf0b0cfc90d8b4be14e00114827494ed5522e9aa1c7e6960515b58626cad0b44",
     "e34efeb4b9538a949655b788dcb517f4a82e997e9e95271ecd392ac073fe216d",
@@ -13,28 +14,33 @@ PASSWORDS_TO_BRUTE_FORCE = [
     "1273682fa19625ccedbe2de2817ba54dbb7894b7cefb08578826efad492f51c9",
     "7e8f0ada0a03cbee48a0883d549967647b3fca6efeb0a149242f19e4b68d53d6",
     "e5f3ff26aa8075ce7513552a9af1882b4fbc2a47a3525000f6eb887ab9622207",
-]
+}
 
 
 def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
-def brute_force_password() -> None:
-    hashs = set(PASSWORDS_TO_BRUTE_FORCE)
-
-
-    for number in range(100_000_000):
+def range_numbers(start: int, end: int):
+    for number in range(start, end):
         fill = str(number).zfill(8)
         result = sha256_hash_str(fill)
 
-        if result in hashs:
+        if result in PASSWORDS_TO_BRUTE_FORCE:
             print(fill)
 
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    brute_force_password()
-    end_time = time.perf_counter()
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        tasks = [
+            executor.submit(range_numbers, 0, 25_000_000),
+            executor.submit(range_numbers, 25_000_000, 50_000_000),
+            executor.submit(range_numbers, 50_000_000, 75_000_000),
+            executor.submit(range_numbers, 75_000_000, 100_000_000),
+        ]
+        for task in tasks:
+            task.result()
 
+    end_time = time.perf_counter()
     print("Elapsed:", end_time - start_time)
